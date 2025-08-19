ec87ea52049f9d0ab936f7d4d4a679349b6b8286
unknown <jeremias.ramirez@davinci.edu.ar>
2025-08-19 10:16:07 -0300

    actividad 2

--- actividad2/index.js
+++ actividad2/index.js
@@ -1,12 +1,28 @@
-const ProductManager = require('./Products.js');
+const Product = require('./Product');
 
-const manager = new ProductManager();
+const model = new Product();
+console.log('inicio de la promesa');
+model.getProducts().then(lista =>{
+    console.table(lista);
+})
+console.log('fin de la promesa');
 
-manager.addProduct({
-    title: 'Teclado',
+async function buscar(id){
+    const product = await model.getProductById(id);
+    console.log(product);
+}
+
+buscar('609a07fb-3376-4bde-8941-63ed56a8946e');
+/*model.addProduct ( {
+    name: 'Teclado dos',
     description: 'Teclado Mecánico',
     price: 25000,
     stock: 25
 });
 
-console.log(manager.getProducts());
+model.addProduct ( {
+    name: 'Mouse',
+    description: 'Mouse',
+    price: 15000,
+    stock: 20
+}); */
